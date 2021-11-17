b77761b9fcbe1b00aed029f69d50d26e99b670a7
tokudaek <tokudaek@gmail.com>
2021-11-17 10:05:18 -0300

    Update plots

--- src/plots.py
+++ src/plots.py
@@ -20,27 +20,45 @@ def main(outdir):
     info(inspect.stack()[0][3] + '()')
 
     respath = './data/results.csv'
+    # respath = '/home/frodo/results/geometricgraph/20211108-geom/uniform/results.csv'
     df = pd.read_csv(respath)
+    steps = np.unique(df.step)
+    alphas = np.unique(df.alpha)
 
     W = 1200; H = 960
     spl = 10 # Plot every spl points
 
+    # Plot evolution in time of each feature
     for feat in ['vcount', 'acount', 'recipr', 'k']:
         fig, ax = plt.subplots(figsize=(W*.01, H*.01), dpi=100)
-
-        for a in sorted(np.unique(df.alpha)):
+        xs = steps[::spl]
+        for a in sorted(alphas):
             df2 = df.loc[df.alpha == a]
-            xs = np.unique(df2.step.values)[::spl]
             ys = df2.groupby(['step'])[feat].mean()[::spl]
             yerr = df2.groupby(['step'])[feat].std()[::spl]
+            # ax.plot(xs, np.power(ys, 3), alpha=.5, label=a)
             ax.errorbar(xs, ys, yerr, alpha=.5, label=a)
             ax.set_ylabel(feat.capitalize())
             ax.set_xlabel('Time')
-            outpath = pjoin(outdir, feat + '.pdf')
+        outpath = pjoin(outdir, feat + '.pdf')
         plt.legend()
         plt.savefig(outpath); plt.close()
-    info('For Aiur!')
 
+    # Write to csv
+    spl = 100 # Plot every spl points
+    for feat in ['vcount', 'recipr', 'k']:
+        data = []
+        xs = steps[::spl]
+        for a in sorted(alphas):
+            df2 = df.loc[df.alpha == a]
+            ys = df2.groupby(['step'])[feat].mean()[::spl]
+            yerr = df2.groupby(['step'])[feat].std()[::spl]
+            data.append(ys)
+        data = np.array(data).T
+        df3 = pd.DataFrame(data, columns=[str(a) for a in alphas])
+        print('##########################################################')
+        print(feat)
+        print(df3.to_latex())
 
 ##########################################################
 if __name__ == "__main__":
