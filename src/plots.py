a05b1e1a7fe32854995fdfd2130ca1cfdef22f50
tokudaek <tokudaek@gmail.com>
2021-12-08 11:44:47 -0300

    Refactor code of plotting

--- src/plots.py
+++ src/plots.py
@@ -11,9 +11,14 @@ import sys
 import numpy as np
 import matplotlib; matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+from matplotlib import cm
 from myutils import info, create_readme
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from scipy.optimize import curve_fit, leastsq
 
+##########################################################
+W = 1200; H = 960
 ##########################################################
 def get_dir_max(losses, ind0):
     """Get the neighbouring pixel which corresponds to the largest gradient
@@ -63,171 +68,168 @@ def gradient_descent(losses, ind0, lr0):
 
         curloss = losses[ind[0], ind[1]]
         curerr = np.abs(losstgt - curloss)
-        # print(curloss)
         step += 1
-    print('predind:{}'.format(ind))
+    return ind
 
+##########################################################
+def plot_features_across_time(df, feats, outdir):
+    """Plot feature across time for each feature"""
+    info(inspect.stack()[0][3] + '()')
+    spl = 10 # Plot every spl points
+    for feat in feats:
+        fig, ax = plt.subplots(figsize=(W*.01, H*.01), dpi=100)
+        xs = np.unique(df.step)[::spl]
+        for a in sorted(np.unique(df.alpha)):
+            df2 = df.loc[df.alpha == a]
+            ys = df2.groupby(['step'])[feat].mean()[::spl]
+            yerr = df2.groupby(['step'])[feat].std()[::spl]
+            ax.plot(xs, np.power(ys, 3), alpha=.5, label=a)
+            ax.errorbar(xs, ys, yerr, alpha=.5, label=a)
+            ax.set_ylabel(feat.capitalize())
+            ax.set_xlabel('Time')
+        outpath = pjoin(outdir, feat + '.pdf')
+        plt.legend()
+        plt.savefig(outpath); plt.close()
+
+##########################################################
+def print_features_latex(df, feats):
+    """Output the features, as requested by Luc"""
+    info(inspect.stack()[0][3] + '()')
+    spl = 300
+    for feat in feats:
+        data = []
+        xs = np.unique(df.step)[::spl]
+        for a in np.unique(df.alpha):
+            df2 = df.loc[df.alpha == a]
+            ys = df2.groupby(['step'])[feat].mean()[::spl]
+            data.append(ys)
+        data = np.array(data).T # cols:alphas, rows:time
+        df3 = pd.DataFrame(data, columns=[str(a) for a in np.unique(df.alpha)])
+        print(df3.to_latex())
+
+##########################################################
+def get_average_values(df, feats, spl):
+    data = []
+    for feat in feats:
+        xs = np.unique(df.step)[::spl]
+        d = []
+        for a in sorted(np.unique(df.alpha)):
+            df2 = df.loc[df.alpha == a]
+            ys = df2.groupby(['step'])[feat].mean()[::spl]
+            d.append(ys)
+        data.append(np.array(d).T)
+    smpvals = {'alpha' : np.unique(df.alpha),
+            'step': xs}
+    return np.array(data), smpvals
+
+##########################################################
+def get_loss(data, tgt, outdir):
+    """Plot loss given by least squared difference"""
+    info(inspect.stack()[0][3] + '()')
+
+    losses = np.zeros((data.shape[1], data.shape[2]))
+    for i in range(data.shape[0]):
+        losses += np.power(data[i, :, :] - tgt[i], 2)
+
+    # Plot loss map
+    loss = losses[::300]
+    scaler = MinMaxScaler((0, 255))
+    normloss = scaler.fit_transform(loss).astype(int)
+    plt.imshow(normloss)
+    plt.colorbar()
+    plt.savefig(pjoin(outdir, 'loss.png'))
+    return losses
+
+##########################################################
+def find_minimum(losses, smpvals, outdir):
+    """Find the minimum (interpolate)"""
+    info(inspect.stack()[0][3] + '()')
+    minind = np.unravel_index(np.argmin(losses), losses.shape)
+    steppred = smpvals['step'][minind[0]]
+    alphapred = smpvals['alpha'][minind[1]]
+    print('Min step, pred:', steppred, alphapred)
+    print('Minind:{}'.format(minind))
+
+    ind0 = (5, 5)
+    lr0 = 5
+    predind = gradient_descent(losses, ind0, lr0)
+
+##########################################################
+def plot_wireframe3d(xs, ys, zs, outpath):
+    """Short description """
+    info(inspect.stack()[0][3] + '()')
+    # xx2, yy2 = np.meshgrid(alphas, times)
+    
+    # zz2 = fitfun((xx2, yy2), *popt)
+    # fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+    # surf = ax.plot_wireframe(xx2, yy2, data-zz2)
+    # plt.savefig('/tmp/' + feat + '.png'); plt.close()
+    
+
+    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+    surf = ax.plot_wireframe(xs, ys, zs)
+    plt.savefig(outpath); plt.close()
+
+##########################################################
+def plot_scatter3d(x_data, y_data, z_data):
+    fig, axes = plt.subplots(subplot_kw={"projection": "3d"})
+    axes.scatter(x_data, y_data, z_data)
+    axes.set_title('Scatter Plot (click-drag with mouse)')
+    axes.set_xlabel('alpha')
+    axes.set_ylabel('step')
+    axes.set_zlabel(feat)
+    plt.savefig(pjoin(outdir, feat + '.png')); plt.close('all')
+
+##########################################################
+
+##########################################################
+def fit_polynomials(deg, xs, ys, data, feats, outdir):
+    """Short description """
+    info(inspect.stack()[0][3] + '()')
+    xflat = xs.flatten()
+    yflat = ys.flatten()
+    p0 = [1] * np.power(deg + 1, 2)
+
+    def poly(xy, *coeffs):
+        x, y = xy
+        k = 0
+        s = 0 
+        for i in range(deg + 1):
+            for j in range(deg + 1):
+                s += coeffs[k] * np.power(x, i) * np.power(y, j)
+                k += 1
+        return s
+
+    for i, feat in enumerate(feats):
+        zs = data[i, :, :]
+        zflat = zs.flatten()
+        popt, pcov = curve_fit(poly, (xflat, yflat), zflat, p0)
+        zspred = poly((xs, ys), *popt)
+        
+        plot_wireframe3d(xs, ys, zs, pjoin(outdir, feat + '_orig_wframe.png'))
+        plot_wireframe3d(xs, ys, zspred, pjoin(outdir, feat + '_pred_wframe.png'))
+        plot_wireframe3d(xs, ys, zs - zspred, pjoin(outdir, feat + '_diff_wframe.png'))
 
 ##########################################################
 def main(outdir):
     """Short description"""
     info(inspect.stack()[0][3] + '()')
 
+    feats = ['vcount', 'recipr', 'k']
+
     respath = './data/results.csv'
-    # respath = '/home/frodo/results/geometricgraph/20211108-geom/uniform/results.csv'
     df = pd.read_csv(respath)
-    steps = np.unique(df.step)
-    alphas = np.unique(df.alpha)
 
-    W = 1200; H = 960
-    spl = 10 # Plot every spl points
+    # plot_features_across_time(df, feats, outdir)
+    # print_features_latex(df, feats)
+    data, smpvals = get_average_values(df, feats, 300)
+    # losses = get_loss(data, tgt=[400, .25, 3], outdir=outdir)
+    # lossmin = find_minimum(losses, smpvals, outdir)
+    xs, ys = np.meshgrid(smpvals['alpha'], smpvals['step'])
 
-    # Plot evolution in time of each feature
-    # for feat in ['vcount', 'acount', 'recipr', 'k']:
-        # fig, ax = plt.subplots(figsize=(W*.01, H*.01), dpi=100)
-        # xs = steps[::spl]
-        # for a in sorted(alphas):
-            # df2 = df.loc[df.alpha == a]
-            # ys = df2.groupby(['step'])[feat].mean()[::spl]
-            # yerr = df2.groupby(['step'])[feat].std()[::spl]
-            # ax.plot(xs, np.power(ys, 3), alpha=.5, label=a)
-            # ax.errorbar(xs, ys, yerr, alpha=.5, label=a)
-            # ax.set_ylabel(feat.capitalize())
-            # ax.set_xlabel('Time')
-        # outpath = pjoin(outdir, feat + '.pdf')
-        # plt.legend()
-        # plt.savefig(outpath); plt.close()
+    # fit_polynomials(2, xs, ys, data, feats, outdir)
+    fit_polynomials(3, xs, ys, data, feats, outdir)
 
-    
-    # Print latex
-    spl = 300
-    for feat in ['vcount', 'recipr', 'k']:
-        data = []
-        xs = steps[::spl]
-        for a in sorted(alphas):
-            df2 = df.loc[df.alpha == a]
-            ys = df2.groupby(['step'])[feat].mean()[::spl]
-            # yerr = df2.groupby(['step'])[feat].std()[::spl]
-            data.append(ys)
-        data = np.array(data).T # cols:alphas, rows:time
-        df3 = pd.DataFrame(data, columns=[str(a) for a in alphas])
-        # print(df3.to_latex())
-        from scipy.optimize import curve_fit, leastsq
-
-        # def func1(x, a, b, c): return a * np.exp(-b * x) + c
-        # def func2(x, a, b, c):
-            # breakpoint()
-            # return a * x + b
-
-        def func3(xy, a, b, c, d, e, f):
-            x, y = xy
-            return a * np.power(x, 2)  + b * np.power(y, 2) + \
-                c * x * y + d * x + e * y + f
-
-
-        alphas = np.unique(df.alpha)
-        times = steps[::spl]
-        xx, yy = np.meshgrid(alphas, times)
-        xx = xx.flatten()
-        yy = yy.flatten()
-        zz = data.flatten()
-        ind = np.vstack((xx, yy))
-        # dep = zz.reshape(-1, 1)
-        dep = zz
-        p0 = [1, 1, 1, 1, 1, 1]
-        popt, pcov = curve_fit(func3, (xx, yy), zz, p0)
-        
-        # popt, pcov = leastsq(func2, (xx, yy), zz, p0)
-        # popt, pcov = curve_fit(func2, ind, dep)
-
-        # from sklearn.linear_model import LinearRegression
-        # from sklearn.preprocessing import PolynomialFeatures
-        # X = np.vstack((xx, yy)).T
-        # y = zz
-        # reg = LinearRegression()
-        # reg = PolynomialFeatures(degree=2)
-        # reg = reg.fit(X, y)
-
-        from matplotlib import cm
-        from matplotlib.ticker import LinearLocator
-
-        def ScatterPlot(x_data, y_data, z_data):
-            fig, axes = plt.subplots(subplot_kw={"projection": "3d"})
-            # f = plt.figure()
-
-            # matplotlib.pyplot.grid(True)
-            # from mpl_toolkits.mplot3d import  Axes3D
-            # axes = Axes3D(f)
-
-            axes.scatter(x_data, y_data, z_data)
-
-            axes.set_title('Scatter Plot (click-drag with mouse)')
-            axes.set_xlabel('alpha')
-            axes.set_ylabel('step')
-            axes.set_zlabel(feat)
-
-            plt.savefig(pjoin(outdir, feat + '.png'))
-            plt.close('all')
-
-        ScatterPlot(xx, yy, zz)
-
-
-        # Plot the surface.
-        xx, yy = np.meshgrid(alphas, times)
-        zz = func3((xx, yy), *popt)
-
-        fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-        # surf = ax.plot_surface(xx, yy, zz, cmap=cm.coolwarm,
-                               # linewidth=0, antialiased=False)
-        surf = ax.plot_wireframe(xx, yy, zz)
-                               # linewidth=0, antialiased=False)
-
-        fig.colorbar(surf, shrink=0.5, aspect=5)
-        plt.savefig(pjoin(outdir, '{}_3d.png'.format(feat)))
-        plt.close()
-
-    # Plot image req. by luciano
-    spl = 1 # Plot every spl points
-    data = []
-    for feat in ['vcount', 'recipr', 'k']:
-        xs = steps[::spl]
-        d = []
-        for a in sorted(alphas):
-            df2 = df.loc[df.alpha == a]
-            ys = df2.groupby(['step'])[feat].mean()[::spl]
-            # yerr = df2.groupby(['step'])[feat].std()[::spl]
-            d.append(ys)
-        data.append(np.array(d).T)
-    data = np.array(data)
-
-    tgt = [400, .25, 3] # nvert, recipr, k
-
-    losses = np.zeros((data.shape[1], data.shape[2]))
-    for i in range(data.shape[0]):
-        losses += np.power(data[i, :, :] - tgt[i], 2)
-
-
-    # Plot loss map
-    loss = losses[::300]
-    from sklearn.preprocessing import MinMaxScaler
-    scaler = MinMaxScaler((0, 255))
-    normloss = scaler.fit_transform(loss).astype(int)
-    plt.imshow(normloss)
-    plt.colorbar()
-    plt.savefig('/tmp/loss.png')
-
-
-    ind = np.unravel_index(np.argmin(losses, axis=None), losses.shape)
-    steppred = steps[ind[0]]
-    alphapred = alphas[ind[1]]
-
-    print(steppred, alphapred)
-
-    print('minind:{}'.format(ind))
-
-    ind0 = (5, 5)
-    lr0 = 5
-    gradient_descent(losses, ind0, lr0)
 
 ##########################################################
 if __name__ == "__main__":
