f97468aba225411a60a4a51dcd749770b3a541de
tokudaek <tokudaek@gmail.com>
2021-12-09 19:40:22 -0300

    Update minimum

--- src/plots.py
+++ src/plots.py
@@ -19,9 +19,10 @@ from scipy.optimize import curve_fit, leastsq
 
 ##########################################################
 W = 1200; H = 960
+
 ##########################################################
-def get_dir_max(losses, ind0):
-    """Get the neighbouring pixel which corresponds to the largest gradient
+def get_dir_minmax(losses, ind0, optm='max'):
+    """Get the neighbouring pixel which corresponds to the minimum/maximum gradient
     magnitude"""
     ind0 = np.array(ind0)
     curloss = losses[ind0[0], ind0[1]]
@@ -31,45 +32,68 @@ def get_dir_max(losses, ind0):
         [+ 1,   0 ], [  0, + 1 ],
         [- 1,   0 ], [  0, - 1 ], ])
 
-    maxdiff = 0
-    dirmax = ind0
+    refdiff = 99999999 if optm == 'min' else 0
+    refdir = ind0
     h, w = losses.shape
 
     for dir_ in dirs_:
         i, j = np.array(ind0 + dir_)
         if (i < 0) or (i >= h) or (j < 0) or (j >= w): continue
         curdiff = losses[i, j] - curloss
-        if curdiff > maxdiff:
-            dirmax = dir_
-            maxdiff = curdiff
-    return dirmax
+        if (optm == 'min' and curdiff < refdiff) or (optm == 'max' and curdiff > refdiff):
+            refdir = dir_
+            refdiff = curdiff
+    return refdir
+
+##########################################################
+def get_dir(losses, ind, method='greedy'):
+    """Choose the 8-connected direction based on neighbourhood given that we are
+    trying to minimize the losses"""
+    if method == 'greedy':
+        return get_dir_minmax(losses, ind, 'min')
+    elif method == 'opposmax':
+        return - get_dir_minmax(losses, ind, 'max')
 
 ##########################################################
 def gradient_descent(losses, ind0, lr0):
     errthresh = 1e-3 # Error threshold
     maxsteps = 1000
     curerr = 99999
-    step = 0
     lr = lr0
     losstgt = 0 # Target loss
 
     h, w = losses.shape
 
+    visitted = [] # Store the descent path
+    step = 0
     ind = ind0
     while (step < maxsteps) and (curerr > errthresh):
-        dirmax =  get_dir_max(losses, ind0)
-        ind = ind - lr * dirmax
-        ind = np.array([int(np.round(ind[0])), int(np.round(ind[1]))])
+        lr *= .8
+        indflat = np.ravel_multi_index(ind, (h, w))
+        # if indflat in visitted: break # It is loop, unless the learning rate is different
+        visitted.append(indflat)
 
-        if ind[0] < 0: ind[0] = 0 # Clip values
+        # ind = np.unravel_index(indflat, (h, w))
+
+        ind = ind + lr * get_dir(losses, ind, 'opposmax')
+        ind = np.array([int(np.round(ind[0])), int(np.round(ind[1]))])
+        
+
+        if ind[0] < 0: ind[0] = 0  # Clip values in the borders
         elif ind[0] > h: ind[0] = h - 1
         if ind[1] < 0: ind[1] = 0
         elif ind[1] >= w: ind[1] = w - 1
 
         curloss = losses[ind[0], ind[1]]
         curerr = np.abs(losstgt - curloss)
+        print(curloss)
+
+        newindflat = np.ravel_multi_index(ind, (h, w))
+        if newindflat == indflat: break
+
         step += 1
-    return ind
+
+    return ind, np.unravel_index(visitted, (h, w))
 
 ##########################################################
 def plot_features_across_time(df, feats, outdir):
@@ -141,18 +165,63 @@ def get_loss(data, tgt, outdir):
     return losses
 
 ##########################################################
-def find_minimum(losses, smpvals, outdir):
-    """Find the minimum (interpolate)"""
+def get_min(losses, smpvals, outdir):
+    """Get the minimum from the @losses matrix"""
     info(inspect.stack()[0][3] + '()')
+    h, w = losses.shape
     minind = np.unravel_index(np.argmin(losses), losses.shape)
-    steppred = smpvals['step'][minind[0]]
-    alphapred = smpvals['alpha'][minind[1]]
-    print('Min step, pred:', steppred, alphapred)
-    print('Minind:{}'.format(minind))
+    steps = smpvals['step']
+    alphas = smpvals['alpha']
+    stepgt = steps[minind[0]]
+    alphagt = alphas[minind[1]]
+    print('GT: step, alpha, ind, loss:', stepgt, alphagt, minind,
+            losses[minind[0], minind[1]])
 
-    ind0 = (5, 5)
-    lr0 = 5
-    predind = gradient_descent(losses, ind0, lr0)
+    deg = 2
+    def poly(xy, *coeffs):
+        x, y = xy
+        k = 0
+        s = 0 
+        for i in range(deg + 1):
+            for j in range(deg + 1):
+                s += coeffs[k] * np.power(x, i) * np.power(y, j)
+                k += 1
+        return s
+
+    i0, j0 = minind 
+    if i0 > 0 and i0 < h - 1 and j0 > 0 and j0 < w - 1:
+        dir_ = get_dir_minmax(losses, minind, optm='min')
+        # print(losses[i0-1, i0)
+        breakpoint()
+            
+        # xx = np.array([steps[i0 - 1], steps[i0], steps[i0 + 1]])
+        # yy = np.array([alphas[j0 - 1], alphas[j0], alphas[j0 + 1]])
+        # xs, ys = np.meshgrid(xx, yy)
+        # aux = np.array([-1, 0, +1])
+        # ii, jj = np.meshgrid(aux, aux)
+        # zflat = losses[ii, jj].flatten()
+        # p0 = [1] * np.power(deg + 1, 2)
+        # popt, pcov = curve_fit(poly, (xs.flatten(), ys.flatten()), zflat, p0)
+        # breakpoint()
+    
+
+    return minind
+
+##########################################################
+def estimate_min(losses, smpvals, outdir):
+    # TODO: Try to walk in the direction of min increase
+
+    ind0 = (7, 1)
+    lr0 = 10 
+    predind, pathinds = gradient_descent(losses, ind0, lr0)
+    # print(len(pathinds))
+    steppred = smpvals['step'][predind[0]]
+    alphapred = smpvals['alpha'][predind[1]]
+    print('PRED: step, alpha, ind, loss:', steppred, alphapred, predind,
+            losses[predind[0], predind[1]])
+    breakpoint()
+    
+    return predind
 
 ##########################################################
 def plot_wireframe3d(xs, ys, zs, outpath):
@@ -168,19 +237,18 @@ def plot_wireframe3d(xs, ys, zs, outpath):
 
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
     surf = ax.plot_wireframe(xs, ys, zs)
+    ax.set_xlabel('alpha')
+    ax.set_ylabel('step')
     plt.savefig(outpath); plt.close()
 
 ##########################################################
-def plot_scatter3d(x_data, y_data, z_data):
+def plot_scatter3d(x_data, y_data, z_data, outpath):
     fig, axes = plt.subplots(subplot_kw={"projection": "3d"})
     axes.scatter(x_data, y_data, z_data)
-    axes.set_title('Scatter Plot (click-drag with mouse)')
     axes.set_xlabel('alpha')
     axes.set_ylabel('step')
-    axes.set_zlabel(feat)
-    plt.savefig(pjoin(outdir, feat + '.png')); plt.close('all')
-
-##########################################################
+    # axes.set_zlabel(feat)
+    plt.savefig(outpath); plt.close('all')
 
 ##########################################################
 def fit_polynomials(deg, xs, ys, data, feats, outdir):
@@ -206,9 +274,47 @@ def fit_polynomials(deg, xs, ys, data, feats, outdir):
         popt, pcov = curve_fit(poly, (xflat, yflat), zflat, p0)
         zspred = poly((xs, ys), *popt)
         
-        plot_wireframe3d(xs, ys, zs, pjoin(outdir, feat + '_orig_wframe.png'))
-        plot_wireframe3d(xs, ys, zspred, pjoin(outdir, feat + '_pred_wframe.png'))
-        plot_wireframe3d(xs, ys, zs - zspred, pjoin(outdir, feat + '_diff_wframe.png'))
+        # plot_wireframe3d(xs, ys, zs, pjoin(outdir, feat + '_orig_wframe.png'))
+        plot_scatter3d(xs, ys, zs, pjoin(outdir, feat + '_1orig_wframe.png'))
+        plot_wireframe3d(xs, ys, zspred, pjoin(outdir, feat + '_2pred_wframe.png'))
+        plot_wireframe3d(xs, ys, zs - zspred, pjoin(outdir, feat + '_3diff_wframe.png'))
+
+##########################################################
+def get_ranges(df, feats):
+    franges = []
+    for feat in feats:
+        uvals = np.unique(df[feat])
+        franges.append([np.min(uvals), np.max(uvals)])
+    return franges
+
+##########################################################
+def interpolate_one_axis(data, smpvals):
+    """Short description """
+    info(inspect.stack()[0][3] + '()')
+    dd = data[0, :, :]
+    h, w  = dd.shape
+    vtgt = 400
+    diff = dd - vtgt
+    inds = np.where(diff == np.min(np.abs(diff)))
+    ic, jc = [inds[0][0], inds[1][0]] # Get just the first one
+    
+    print(ic, jc)
+
+    if ic == 0 or ic == h - 1: #TODO: fix this later
+        s2 = smpvals['step'][ic]
+    else:
+        i1 = ic - 1; i3 = ic + 1
+        s1 = smpvals['step'][i1]; s3 = smpvals['step'][i3];
+        v1 = dd[i1, jc]; v2 = vtgt; v3 = dd[i3, jc]
+        s2 = (v2-v1) / (v3-v1) * (s3-s1) + s1
+
+    if jc == 0 or jc == w - 1: #TODO: fix this later
+        a2 = smpvals['alpha'][jc]
+    else:
+        j1 = jc - 1; j3 = jc + 1
+        a1 = smpvals['alpha'][j1]; a3 = smpvals['alpha'][j3];
+        v1 = dd[i1, jc]; v2 = vtgt; v3 = dd[i3, jc]
+        a2 = (v2-v1) / (v3-v1) * (a3-a1) + a1
 
 ##########################################################
 def main(outdir):
@@ -220,16 +326,28 @@ def main(outdir):
     respath = './data/results.csv'
     df = pd.read_csv(respath)
 
+    franges = get_ranges(df, feats)
     # plot_features_across_time(df, feats, outdir)
     # print_features_latex(df, feats)
-    data, smpvals = get_average_values(df, feats, 300)
-    # losses = get_loss(data, tgt=[400, .25, 3], outdir=outdir)
-    # lossmin = find_minimum(losses, smpvals, outdir)
+    # data, smpvals = get_average_values(df, feats, 1);  interpolate_one_axis(data, smpvals)
+
+    data, smpvals = get_average_values(df, feats, 100)
+    losses = get_loss(data, tgt=[300, .25, 3], outdir=outdir)
+    minidx = get_min(losses, smpvals, outdir)
+    lossmin = estimate_min(losses, smpvals, outdir)
+    breakpoint()
+    
+    return
+    
+    # data, smpvals = get_average_values(df, feats, 300)
+    # data = data[:, 1:, :]
+    # smpvals['step'] =  smpvals['step'][1:]
     xs, ys = np.meshgrid(smpvals['alpha'], smpvals['step'])
 
-    # fit_polynomials(2, xs, ys, data, feats, outdir)
-    fit_polynomials(3, xs, ys, data, feats, outdir)
-
+    os.makedirs(pjoin(outdir, 'poly2'), exist_ok=True)
+    fit_polynomials(2, xs, ys, data, feats, pjoin(outdir, 'poly2'))
+    os.makedirs(pjoin(outdir, 'poly3'), exist_ok=True)
+    fit_polynomials(3, xs, ys, data, feats, pjoin(outdir, 'poly3'))
 
 ##########################################################
 if __name__ == "__main__":
